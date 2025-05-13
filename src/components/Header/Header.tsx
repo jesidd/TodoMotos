8558df48159f68fa692c0781c04d4af78f5366cb
jefersonfloz <jeffersonflorez73@gmail.com>
2025-05-13 01:00:18 -0500

    Componente Header Terminado.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,24 +12,24 @@ const Header: React.FC = () => {
       </div>
 
 
-      <div className="w-full h-[40px] grid grid-cols-3 py-[30px] px-[20px] content-center justify-items-center text-base">
+      <div className="w-full h-[40px] grid grid-cols-3 py-[34px] px-[20px] content-center justify-items-center text-base">
         <div className="flex size-4 justify-star w-60 items-center self-center justify-self-end">
             <img src={lupa} alt="Icono lupa"  className="size-5"/>
         </div>
 
         <nav className="flex w-full justify-center ">
           <ul className="flex items-center justify-around  w-[100%]">
-            <li className="size-auto after:content-[''] after:bg-[#3F444B] after:w-max after:h-[3px] after:absolute after:left-0"><a className="a-estilo" href="#">Inicio</a></li>
-            <li className=" "><a className="a-estilo" href="#">Contacto</a></li>
-            <li className=" "><a className="a-estilo" href="#">Ranking de Motos</a></li>
-            <li className=" "><a className="a-estilo" href="#">Blog</a></li>
+            <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full"><a className="a-estilo" href="#">Inicio</a></li>
+            <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full "><a className="a-estilo" href="#">Contacto</a></li>
+            <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full "><a className="a-estilo" href="#">Ranking de Motos</a></li>
+            <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full "><a className="a-estilo" href="#">Blog</a></li>
           </ul>
         </nav>
 
         <nav className="flex w-full h-full h-[100%]">
             <ul className="flex items-center justify-center gap-5 w-full">
-                <li className=" "><a className="a-estilo" href="#">Creá tu cuenta</a></li>
-                <li className=" "><a className="a-estilo" href="#">Ingresá</a></li>
+                <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full "><a className="a-estilo" href="#">Creá tu cuenta</a></li>
+                <li className="relative after:content-[''] after:bg-[#3F444B] after:w-0 after:h-[3px] after:top-[81%] after:left-0 after:absolute after:rounded-xl after:duration-300 hover:after:w-full "><a className="a-estilo" href="#">Ingresá</a></li>
             </ul>
         </nav>
       </div>
