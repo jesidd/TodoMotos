abbcbb58a69237c556cfdd332f0e580c8d7d119f
Deivis Olascoaga <deivisalcedo.o@gmail.com>
2025-06-04 19:57:23 -0500

    renderizado de footer y header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     < >
       <Home/>
-      <div className='size-32 bg-white'>
+      <div className='size-90 bg-white'>
 
       </div>
+      <Footer/>
     </>
   )
 }
