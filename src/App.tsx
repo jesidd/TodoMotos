451ca3bb21051bfc1fe487e30fd84c9ebfa7d354
Deivis Olascoaga <deivisalcedo.o@gmail.com>
2025-06-04 23:57:33 -0500

    Arreglo de paginacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import Home from './pages/Home/Home'
-import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     < >
       <Home/>
-      <div className='size-90 bg-white'>
-
-      </div>
-      <Footer/>
     </>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Home: React.FC = () => {
   return (
-    <Header/>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 };
 
