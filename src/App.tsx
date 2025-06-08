bbe7fb11219b44e2b5fa73a9c3d4b0114a2e05ee
jefersonfloz <jeffersonflorez73@gmail.com>
2025-06-07 19:57:43 -0500

    ajustando banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import Home from './pages/Home/Home'
 import './App.css'
-import Footer from './components/Footer/Footer'
-import Banner from './components/Banner/Banner'
 
 function App() {
   return (
     < >
       <Home/>
-      <Banner/>
-      <Footer/> 
     </>
   )
 }
