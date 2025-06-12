50b81c9692426b6c39067f89f4e7f1d6d477164b
Deivis Olascoaga <deivisalcedo.o@gmail.com>
2025-06-11 23:26:53 -0500

    Creacion Interface ArticleData

---
+++ src/components/Blog/ArticleData.tsx
@@ -0,0 +1,19 @@
+export interface Moto {
+  id: number;
+  marca: string;
+  modelo: string;
+  año: number;
+  tipo: string;
+  cilindrajeCC: number;
+  potenciaHP: number;
+  torqueNm: number;
+  motor: string;
+  refrigeracion: string;
+  medidaNeumaticoDelantero: number;
+  medidaNeumaticoTrasero: number;
+  transmision: string;
+  pesoKg: number;
+  capacidadCombustibleL: number;
+  imagenUrl: string;
+  descripcion: string;
+}
